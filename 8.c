7901a9d6d6afad4215a26b990659d2bc62b0b5e4
Asmahul Husna <116778793+ekra024@users.noreply.github.com>
2024-06-03 09:50:27 +0600

    Add files via upload

---
+++ 8.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    n=-6;
+    if(n>=0)
+    {
+        printf("The number is positive");
+
+    }
+    else
+    {
+        printf("The number is negative");
+    }
+    return 0;
+}
